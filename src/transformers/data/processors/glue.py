9512bea74c6c56e89f3d90c7c2b7525067eb9cc2
Ziyan Lai <ZiyanLai@ZiyandeMacBook-Pro.local>
2020-03-11 12:39:40 -0400

    debug jason

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -547,8 +547,8 @@ class BoolqProcessor(DataProcessor):
         for (i, line) in enumerate(lines):
             if i == 0:
                 continue
-            guid = "%s-%s" % (set_type, int(line['idx']))
             line = json.loads(line)
+            guid = "%s-%s" % (set_type, line['idx'])
             text_a = line['question']
             text_b = line['passage']
             label = line['label']
