c4380ec032858e589ebad08feca62201b913a488
Ziyan Lai <ZiyanLai@b-10-27-64-197.dynapool.vpn.nyu.edu>
2020-03-11 15:46:25 -0400

    debug jason

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -555,7 +555,7 @@ class BoolqProcessor(DataProcessor):
             text_a = line['question']
             text_b = line['passage']
             label = line['label']
-            print(label)
+            # print(label)
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
