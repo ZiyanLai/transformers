b3464a8ce00251dfa3882eb4c3d893ec6cc4aff0
ZiyanLai <you@example.com>
2020-05-10 00:13:57 -0400

    update

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -96,8 +96,8 @@ def glue_convert_examples_to_features(
         inputs = tokenizer.encode_plus(example.text_a, example.text_b, add_special_tokens=True, max_length=max_length, return_token_type_ids=True)
         input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
         print("this is input ids: ")
-        print(inputs['input_ids'],'\n')
-        print(type(inputs['input_ids']))
+        print(inputs['input_ids'][0])
+        print(type(inputs['input_ids'][0]))
 
         # The mask has 1 for real tokens and 0 for padding tokens. Only real
         # tokens are attended to.
