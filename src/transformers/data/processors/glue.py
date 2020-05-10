987eb124354f166d41e4f8d9c3cd165d917452b3
ZiyanLai <you@example.com>
2020-05-09 21:08:45 -0400

    update

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -596,7 +596,7 @@ class AbuseProcessor(DataProcessor):
     def get_labels(self):
         """See base class."""
         # return ["threat","insult","toxic","IsAbuse"]
-        return [[0,1],[0,1],[0,1],[0,1]]
+        return [0,1]
 
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
@@ -607,7 +607,8 @@ class AbuseProcessor(DataProcessor):
                 continue
             # guid = "%s-%s" % (set_type, line[0])
             text_a = line[1]
-            label = [line[-4],line[-3],line[-2],line[-1]]
+            print(type(line[-1]))
+            label = line[-1]
             examples.append(InputExample(text_a=text_a, label=label))
         return examples
         # return examples
